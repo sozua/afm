9e411a5bbdcdd5f3354739e11cda4563c480afde
Diogo de Souza <diogo@sozua.dev>
2023-12-23 05:52:44 -0300

    feat: add login

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/foolin/goview"
@@ -8,15 +9,57 @@ import (
 )
 
 func (a *App) SetupRoutes(r *gin.Engine) {
-	r.GET("/", a.handleIndex)
-	r.Static("/static", "./static")
-}
-
-func (a *App) handleIndex(c *gin.Context) {
-	err := goview.Render(c.Writer, http.StatusOK, "index", goview.M{
-		"title": "Hello World!",
+	r.GET("/", func(c *gin.Context) {
+		gv := goview.New(goview.Config{
+			Root:      "views/marketing",
+			Extension: ".html",
+			Master:    "layouts/master",
+		})
+		goview.Use(gv)
+		err := goview.Render(c.Writer, http.StatusOK, "index", goview.M{
+			"title": "Hello World!",
+		})
+		if err != nil {
+			panic(err)
+		}
 	})
-	if err != nil {
-		panic(err)
-	}
+	r.GET("/login-modal", func(c *gin.Context) {
+		gv := goview.New(goview.Config{
+			Root:      "views/_shared/partials",
+			Extension: ".html",
+		})
+		goview.Use(gv)
+		err := goview.Render(c.Writer, http.StatusOK, "login-modal.html", goview.M{})
+		if err != nil {
+			panic(err)
+		}
+	})
+	r.POST("/login", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		fmt.Println("username: ", username)
+		fmt.Println("password: ", password)
+		if username == "admin" && password == "admin" {
+			c.Redirect(http.StatusMovedPermanently, "/account")
+			return
+		}
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "invalid credentials",
+		})
+	})
+	r.GET("/account", func(c *gin.Context) {
+		gv := goview.New(goview.Config{
+			Root:      "views/app",
+			Extension: ".html",
+			Master:    "layouts/master",
+		})
+		goview.Use(gv)
+		err := goview.Render(c.Writer, http.StatusOK, "user/account", goview.M{
+			"title": "My account!",
+		})
+		if err != nil {
+			panic(err)
+		}
+	})
+	r.Static("/static", "./static")
 }
